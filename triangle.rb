5dbe4ee1df946e21c15da902b81fd1c3eb92b336
ryclorak <ryclorak@gmail.com>
2023-01-03 13:07:45 -0800

    add moving square
    added wasd controls for a white square on the triangle, with color
    changing background

--- triangle.rb
+++ triangle.rb
@@ -2,14 +2,38 @@ require 'ruby2d'
 
 set title: 'mothafuckin triangle', background: 'fuchsia'
 
+tick = 0
+@x_speed = 0
+@y_speed = 0
+
 Triangle.new(
     x1: 320, y1:  50,
     x2: 540, y2: 430,
     x3: 100, y3: 430,
     color: ['red', 'green', 'blue']
 )
-tick = 0
+
+@square = Square.new(x: 50, y: 50, size: 100)
+
+on :key_down do |event|
+    if event.key == 'a'
+        @x_speed = -2
+        @y_speed = 0
+    elsif event.key == 'd'
+        @x_speed = 2
+        @y_speed = 0
+    elsif event.key == 'w'
+        @x_speed = 0
+        @y_speed = -2
+    elsif event.key == 's'
+        @x_speed = 0
+        @y_speed = 2
+    end
+end
+
 update do
+    @square.x += @x_speed
+    @square.y += @y_speed
     if tick % 60 == 0
         set background: 'random'
     end
